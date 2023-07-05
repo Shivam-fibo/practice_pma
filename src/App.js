ed5353e3b20055abbcae743fdb08648f9ff7bbee
jayeshchauhan8954 <officialjitandrachauhan@gmail.com>
2023-07-05 14:53:44 +0530

    updates by jayesh for sanika

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React, { useEffect } from 'react';
-import NavBar from './components/NavBar/NavBar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Pages/Home.jsx';
-import About from './components/Pages/About.jsx';
-import Blog from './components/Pages/Blog.jsx';
-import Contact from './components/Pages/Contact.jsx';
-import { LoginPage, AboutPage, HomePage } from './routes/Routes';
+import { LoginPage, AboutPage, HomePage, Blog } from './routes/Routes';
 import { useDispatch } from 'react-redux';
 import Editor from './Editor';
 import { pageLoad } from './redux/actions/pageAction';
@@ -21,15 +16,13 @@ function App() {
 	return (
 		<div>
 			<Router>
-				<div className="pages">
-					<Routes>
-						<Route path="/" element={<HomePage />} />
-						<Route path="/blog" element={<Blog />} />
-						<Route path="/auth" element={<LoginPage />} />
-						<Route path="/about" element={<AboutPage />} />
-						<Route path="/editor" element={<Editor />} />
-					</Routes>
-				</div>
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/blog" element={<Blog />} />
+					<Route path="/auth" element={<LoginPage />} />
+					<Route path="/about" element={<AboutPage />} />
+					<Route path="/editor" element={<Editor />} />
+				</Routes>
 			</Router>
 		</div>
 	);
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -2,6 +2,7 @@ import LoginPage from '../pages/loginAndSignupPage/LoginPage.jsx';
 import AboutPage from '../pages/AboutPage/AboutPage.jsx';
 // import HomePage from '../pages/homePage/HomePage.jsx';
 import HomePage from '../pages/HomePage/HomePage.jsx';
+import Blog from '../pages/Blog/Blog.jsx';
 // import About from '../pages/AboutUs/About.jsx';
 // export { LoginPage, HomePage };
-export { LoginPage, AboutPage, HomePage };
+export { LoginPage, AboutPage, HomePage, Blog };
