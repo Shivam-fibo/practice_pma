2882db0ebd0fce45ba11cb42557f700445d8a8e7
abbas <abbash7613@gmail.com>
2023-07-05 13:15:29 +0530

    some ui changes in editor component

--- src/components/grapesJsComp/TopNav.js
+++ src/components/grapesJsComp/TopNav.js
@@ -1,15 +1,21 @@
 import { toggleSidebar } from '../../api_utils/geditor_utils';
+import {useNavigate} from 'react-router-dom'
 
 function TopNav() {
+	const navigate=useNavigate()
 	const handleClick = () => {
-		toggleSidebar(false);
+		// toggleSidebar(false);
+		navigate('/')
 	};
+	
+
 	return (
-		<nav className="navbar navbar-light">
-			<div className="container">
-				<button className="btn btn-sm btn-outline-primary" onClick={handleClick}>
-					<i className="fa fa-bars" />
-				</button>
+		<nav className="d-flex">
+			<button className="ml-5" onClick={handleClick}>
+				<i className="fa fa-arrow-left" />
+			</button>
+			<div className="ml-60">
+				
 				<div className="panel__devices" />
 				<div className="panel__editor" />
 				<div className="panel__basic-actions" />
